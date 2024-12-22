5379b9f27234a596e164813abce11ab7b7248c3e
Mirza Hilmi <dev@mrz.my.id>
2024-12-22 21:41:58 +0700

    refactor: temporarily remove otlp configuration

--- Extensions/Extensions.cs
+++ Extensions/Extensions.cs
@@ -1,7 +1,5 @@
 using Hello.NET.Data;
 using Microsoft.EntityFrameworkCore;
-using OpenTelemetry.Resources;
-using OpenTelemetry.Trace;
 
 namespace Hello.NET.Extensions;
 
@@ -11,25 +9,6 @@ public static class Extensions
         this WebApplicationBuilder builder
     )
     {
-        var otlpConfig = builder.Configuration.GetSection("OpenTelemetry");
-        var serviceName = otlpConfig.GetValue<string>("ServiceName")!;
-        var serviceVersion = otlpConfig.GetValue<string>("ServiceVersion")!;
-        builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseNpgsql(
-                builder.Configuration.GetConnectionString("PrimaryDatabase")
-            )
-        );
-        builder
-            .Services.AddOpenTelemetry()
-            .ConfigureResource(resource =>
-                resource.AddService(
-                    serviceName: serviceName,
-                    serviceVersion: serviceVersion
-                )
-            )
-            .WithTracing(tracing =>
-                tracing.AddAspNetCoreInstrumentation().AddConsoleExporter()
-            );
         builder.Services.AddControllers();
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
@@ -38,6 +17,5 @@ public static class Extensions
                 builder.Configuration.GetConnectionString("PrimaryDatabase")
             )
         );
-        builder.Logging.AddJsonConsole();
     }
 }
