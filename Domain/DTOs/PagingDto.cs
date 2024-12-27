6780ea880415462eaf69a3b7bb5aa0d89addc056
Mirza Hilmi <dev@mrz.my.id>
2024-12-27 09:42:44 +0700

    refactor: formatting

--- Domain/DTOs/PagingDto.cs
+++ Domain/DTOs/PagingDto.cs
@@ -1,6 +1,7 @@
 namespace Hello.NET.Domain.DTOs;
 
-public class PagingDto {
-    public int PageIndex {get; set; } = 1;
-    public int PageSize {get; set; } = 15;
+public class PagingDto
+{
+    public int PageIndex { get; set; } = 1;
+    public int PageSize { get; set; } = 15;
 }
